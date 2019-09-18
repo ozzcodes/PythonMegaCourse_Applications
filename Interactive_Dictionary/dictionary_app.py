fb0421d1c4bfe0de9fa2c9ab9861797f6e87d0f8
OzzyCodes <ajwaldro@gmail.com>
2019-09-18 00:40:05 -0400

    updated dictionary app

--- Interactive_Dictionary/dictionary_app.py
+++ Interactive_Dictionary/dictionary_app.py
@@ -1,26 +1,37 @@
 import json
+import difflib
+from difflib import SequenceMatcher
 
 dictionary_data = json.load(open('resources/dictionary.json'))
+choose_yes = ['yes', 'y']
+choose_no = ['no', 'n']
 
 
 def dictionary_app(get_word):
     if get_word in dictionary_data:
         return dictionary_data[get_word]
-    else:
-        if get_word not in dictionary_data:
-            try_again = input("The word you entered doesn't exist! Would you like to enter another word? (y/n)")
-            print(try_again)
-            if try_again == 'y' or 'Y':
-                new_input = input('Please Enter a word: (Check your spelling)')
-                if new_input == get_word in dictionary_data:
-                    return dictionary_data[new_input]
-            else:
-                "Thank you for using the Dictionary Program!"
+    elif get_word not in dictionary_data:
+        resp = input("The word you entered doesn't exist! Would you like to enter another word? (y/n)").lower().strip()
+        for new_word in resp:
+            if new_word == dictionary_data[new_word]:
+                return dictionary_data[revised_input]
+
+        if resp in choose_yes:
+            input('Please Enter a word: (Check your spelling)')
+            return dictionary_data[revised_input]
+        elif resp in choose_no:
+            # exit loop
+            print('Thank you for using the Python Dictionary Program!')
+            exit(0)
         else:
-            return "Thank you for using the Dictionary Program"
+            pass
+    else:
+        "Thank you for using the Dictionary Program!"
 
 
 get_input = input('Please Enter a word: ')
 user_input = get_input.lower().strip()
+revised_input = input('Please Enter a word: (Check your spelling)')
+new_input = revised_input.lower().strip()
 
-print(dictionary_app(user_input))
+print(dictionary_app(user_input or revised_input))
