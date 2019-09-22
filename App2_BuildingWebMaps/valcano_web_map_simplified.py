0f68f0ec53cea1f04c73ef34f9e35e70d8f9b766
Austin Waldron <waldron.a@husky.neu.edu>
2019-09-21 20:37:09 -0400

    completed web-map application2

--- App2_BuildingWebMaps/valcano_web_map_simplified.py
+++ App2_BuildingWebMaps/valcano_web_map_simplified.py
@@ -1,5 +1,6 @@
 import folium
 import pandas as pd
+import json
 
 # Read the datafile with all volcano locations in the US
 data = pd.read_csv('data_files/Volcanoes-USA.txt')
@@ -20,11 +21,23 @@ def marker_color(elev):
     return color
 
 
+# Add the feature group created above to be able to filter out the icons in the Layer Control
+feature_group = folium.FeatureGroup(name='Volcano Locations')
+
 # Add markers one by one using the data range defined in your dataset
 for lat, lon, name, elevation in zip(data['LAT'], data['LON'], data['NAME'], data['ELEV']):
-    folium.Marker(location=[lat, lon], popup=name, icon=folium.Icon(marker_color(elevation))).add_to(my_map)
+    feature_group.add_child(folium.Marker(location=[lat, lon], popup=name,
+                                          icon=folium.Icon(marker_color(elevation), icon_color='black'))).add_to(my_map)
+
+geojson_layer = my_map.add_child(
+    folium.GeoJson(json.load(open('data_files/world_population.json')),
+                   name='World Population',
+                   style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] <= 10000000
+                   else 'orange' if 10000000 < x['properties']['POP2005'] <= 20000000 else 'red'}))
+
+layer_controller = my_map.add_child(folium.LayerControl(position='topright', collapsed=True))
 
 # Save the folium map to a directory as an html file
-my_map.save('data_files/us_map.html')
+my_map.save(outfile='data_files/us_map.html')
 
 print('Creating a webmap for Volcanoes in the US...')
