ceaf10ab53675d93abf5fbedc27434ca39725673
OzzyCodes <ajwaldro@gmail.com>
2019-09-25 00:57:26 -0400

    updated application 3 framework

--- App3_Website_AddBlocker/daily_site_blocker.py
+++ App3_Website_AddBlocker/daily_site_blocker.py
@@ -2,16 +2,17 @@ import time
 from datetime import datetime as dt
 
 host_path = r"C:\Windows\System32\drivers\etc\hosts"
+temp_path = "source_file/hosts"
 redirect = '127.0.0.1'
-block_list = ['www.facebook.com', 'www.twitter.com']
+block_list = ['www.facebook.com', 'facebook.com', 'www.twitter.com', 'www.instagram.com']
 final_list = [redirect + " " + i for i in block_list]
 final_string_block = '\n'.join(final_list)
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month,
-                                                                          dt.now().day, 18):
+                                                                          dt.now().day, 16):
         print('Within time...')
-        with open(host_path, "r+") as file:
+        with open(temp_path, 'r+') as file:
             content = file.read()
             for website in block_list:
                 if website in content:
@@ -19,11 +20,12 @@ while True:
                 else:
                     file.write(redirect + " " + website + "\n")
     else:
-        with open(host_path, "r+") as file:
+        with open(temp_path, "r+") as file:
             content = file.readlines()
             file.seek(0)
             for line in content:
                 if not any(website in line for website in block_list):
                     file.write(line)
             file.truncate()
+        print('Fun hours!!')
     time.sleep(3)
