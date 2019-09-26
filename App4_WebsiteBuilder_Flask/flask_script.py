8d3a094464d8d02e4f80b57901d8cc6c140e4608
AWaldron <ajwaldro@gmail.com>
2019-09-26 15:53:50 -0400

    updated webapp with templates for pages

--- App4_WebsiteBuilder_Flask/flask_script.py
+++ App4_WebsiteBuilder_Flask/flask_script.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 my_app = Flask(__name__)
 
@@ -6,9 +6,15 @@ my_app = Flask(__name__)
 # Create a root page for homepage
 @my_app.route('/')
 def home():
-    return 'My website content here please!'
+    return render_template('home.html')
 
 
-# Run main file (my_app) as a debugging script
+# Example root to an about page (although not currently linked)
+@my_app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+# Run main file (my_app) as a development package
 if __name__ == "__main__":
     my_app.run(debug=True)
