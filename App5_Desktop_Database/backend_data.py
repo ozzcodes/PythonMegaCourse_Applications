65900b25b55771582832613232ee83104ed43893
ajwaldro878 <ajwaldro@gmail.com>
2019-10-04 14:41:48 -0400

    application 5 created

---
+++ App5_Desktop_Database/backend_data.py
@@ -0,0 +1,58 @@
+import sqlite3
+import random
+import string
+
+# db_connection = sqlite3.connect('books.sqlite')
+
+
+# Generate random ISBN numbers
+def isbn_generator(size=12, chars=string.ascii_lowercase + string.digits):
+
+    return ''.join(random.choice(chars) for x in range(size))
+
+
+# Create database connection
+def db_connect():
+    conn = sqlite3.connect('books.sqlite')
+    curs = conn.cursor()
+    curs.execute("CREATE TABLE IF NOT EXISTS bookstore ("
+                 "id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn TEXT )")
+    conn.commit()
+    conn.close()
+
+
+# Create function to insert data
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect('books.sqlite')
+    curs = conn.cursor()
+    curs.execute("INSERT INTO bookstore VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+
+# Create a view function
+def view():
+    conn = sqlite3.connect('books.sqlite')
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM bookstore")
+    rows = curs.fetchall()
+    conn.close()
+
+    return rows
+
+
+# Create a search function
+def search(title, author, year, isbn):
+    conn = sqlite3.connect('books.sqlite')
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM bookstore WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+    rows = curs.fetchall()
+    conn.close()
+
+    return rows
+
+
+db_connect()
+
+# insert('The Old Man and the Sea', 'Ernest Hemingway', 1952, isbn=isbn_generator())
+print(view())
