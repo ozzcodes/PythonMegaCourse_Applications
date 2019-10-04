8bcd2339d48f9efcdba9b556cbb80cf2d4193878
Austin Waldron <ajwaldro@gmail.com>
2019-10-03 21:31:10 -0400

    updated working with databases - python with postgres

---
+++ PythonBasics/working_with_databases/postgres_connections.py
@@ -0,0 +1,78 @@
+import psycopg2
+
+
+# First create a table with rows
+def create_table():
+    # Create a connection
+    conn = psycopg2.connect('db.postgres')
+    curs = conn.cursor()
+    curs.execute("CREATE TABLE IF NOT EXISTS Mystore (item MESSAGE_TEXT , quantity INTEGER, price REAL )")
+
+    # Commit database and data to data source and then close connection
+    conn.commit()
+    conn.close()
+
+
+# Enter some data
+def insert(item, quantity, price):
+    conn = psycopg2.connect('db.sqlite')
+    curs = conn.cursor()
+    curs.execute("INSERT INTO Mystore VALUES (?, ?, ?)", (item, quantity, price))
+
+    # Commit database and data to data source and then close connection
+    conn.commit()
+    conn.close()
+
+
+# Insert some random data
+# insert('Waterbottle', 15, 5)
+# insert('Coffee mug', 12, 8)
+
+
+def view():
+    conn = psycopg2.connect('db.sqlite')
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM Mystore")
+    rows = curs.fetchall()
+    conn.commit()
+    conn.close()
+
+    return rows
+
+
+def delete(item):
+    conn = psycopg2.connect('db.sqlite')
+    curs = conn.cursor()
+    # Delete an item
+    curs.execute("DELETE FROM Mystore WHERE item=?", (item,))  # Need a comma for SQLite after parameter
+    conn.commit()
+    conn.close()
+
+
+def update(quantity, price, item):
+    conn = psycopg2.connect('db.sqlite')
+    curs = conn.cursor()
+    # Update an item
+    curs.execute("UPDATE Mystore SET quantity=?, price=? WHERE item=?",
+                 (quantity, price, item))  # Need a comma for SQLite after parameter
+    conn.commit()
+    conn.close()
+
+
+def remove_duplicates():
+    conn = psycopg2.connect('db.sqlite')
+    curs = conn.cursor()
+    curs.execute("DELETE FROM Mystore WHERE item NOT IN (SELECT min(item) FROM Mystore GROUP BY item, quantity)")
+
+
+# Delete an item
+delete('Wine Glass')
+
+# Update an item
+update(12, 18, 'Waterbottle')
+
+# Select non-duplicated items
+remove_duplicates()
+
+# Print a view of the data in a list format
+print(view())
