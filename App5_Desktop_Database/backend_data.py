2099fe494efb53180cbfc1de5cedc99b5f7c408d
ajwaldro878 <ajwaldro@gmail.com>
2019-10-04 14:42:20 -0400

    reformat files

--- App5_Desktop_Database/backend_data.py
+++ App5_Desktop_Database/backend_data.py
@@ -2,12 +2,9 @@ import sqlite3
 import random
 import string
 
-# db_connection = sqlite3.connect('books.sqlite')
-
 
 # Generate random ISBN numbers
 def isbn_generator(size=12, chars=string.ascii_lowercase + string.digits):
-
     return ''.join(random.choice(chars) for x in range(size))
 
 
