45d7c2d428475b921c8432fae85849139a247803
OzzyCodes <ajwaldro@gmail.com>
2019-10-19 14:14:06 -0400

    finalized basic bank account application with OOP

--- PythonBasics/Object_Oriented_Programming/bank_account/account.py
+++ PythonBasics/Object_Oriented_Programming/bank_account/account.py
@@ -3,9 +3,30 @@ class Account:
 
     # Initialize the account
     def __init__(self, filepath):
+        # Create local variable to access file path for later use
+        self.path = filepath
         with open(filepath, 'r') as file:
             self.balance = int(file.read())
 
+    # Create another function for use in class
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
 
-my_account = Account("PythonBasics/Object_Oriented_Programming/bank_account/balance.txt")
+    # Create deposit function and update account balance
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+    # Create a function to save the balance
+    def update_balance(self):
+        with(open(self.path, 'w')) as file:
+            file.write(str(self.balance))
+
+
+my_account = Account('balance.txt')
+
+my_account.withdraw(900)
+print(my_account.balance)
+my_account.update_balance()
+print(my_account.balance)
+my_account.deposit(1250)
 print(my_account.balance)
