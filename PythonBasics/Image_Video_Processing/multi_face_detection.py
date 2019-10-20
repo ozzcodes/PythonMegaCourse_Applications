e958d32db1481fd874f1c343a743ae1a78d52054
OzzyCodes <ajwaldro@gmail.com>
2019-10-20 15:07:35 -0400

    facial recognition image read and write

--- PythonBasics/Image_Video_Processing/multi_face_detection.py
+++ PythonBasics/Image_Video_Processing/multi_face_detection.py
@@ -2,11 +2,11 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('resource_files/haarcascade_frontalface_default.xml')
 
-img = cv2.imread('resource_files/ajw_recognition2.jpg')
+img = cv2.imread('resource_files/Kelly_Austin_Fullbody.jpg')
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Detect the face of image using pixel dimensions
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05,
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.075,
                                       minNeighbors=5)
 
 # create a rectangle around image to detect in pixels
@@ -17,11 +17,8 @@ for x, y, w, h in faces:
 print(type(faces))  # Numpy Array
 print(faces)
 
-resized = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
-cv2.imwrite('resource_files/face_resized.jpg', resized)
+cv2.imwrite('resource_files/face_detect_kelly_austin.jpg', img)
 
-# cv2.imshow('Gray_Image', gray_img)
-# cv2.imshow('Gray_Image', img)
-cv2.imshow('Gray_Image', resized)
+cv2.imshow('Gray_Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
