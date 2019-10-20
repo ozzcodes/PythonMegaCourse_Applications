7dcf33d0a5bd9fe63c2b658114626f73ef3e56c0
OzzyCodes <ajwaldro@gmail.com>
2019-10-20 13:17:33 -0400

    facial recognition code

--- PythonBasics/Image_Video_Processing/face_detection.py
+++ PythonBasics/Image_Video_Processing/face_detection.py
@@ -1,2 +1,11 @@
 import cv2
 
+face_cascade = cv2.CascadeClassifier('resource_files/haarcascade_frontalface_default.xml')
+
+img = cv2.imread('resource_files/ajw_recognition.jpg')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Gray_Image', gray_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
