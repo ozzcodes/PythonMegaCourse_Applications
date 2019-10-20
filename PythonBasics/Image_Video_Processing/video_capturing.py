3045595f3036a2aff0848405117009e2f8909435
OzzyCodes <ajwaldro@gmail.com>
2019-10-20 16:36:35 -0400

    capture video from webcam

--- PythonBasics/Image_Video_Processing/video_capturing.py
+++ PythonBasics/Image_Video_Processing/video_capturing.py
@@ -1,8 +1,12 @@
 import cv2
+import time
 
 # Capture a video form camera using opencv
 # First camera, specify index if multiple camera's
 my_video = cv2.VideoCapture(0)
+
+# Allow the camera to be used for specified time
+time.sleep(3)
 
 # Allow computer to release imaging from camera
 my_video.release()
