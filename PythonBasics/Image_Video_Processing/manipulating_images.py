6560c49db610590c1372c89c35045f3c6a207145
OzzyCodes <ajwaldro@gmail.com>
2019-10-20 11:50:09 -0400

    write image to directory

--- PythonBasics/Image_Video_Processing/manipulating_images.py
+++ PythonBasics/Image_Video_Processing/manipulating_images.py
@@ -12,3 +12,10 @@ print(img.ndim)
 cv2.imshow('Hexacopter', img)
 cv2.waitKey(5000)  # pass time to wait before image window closes or 0 to close upon a key pressed
 cv2.destroyAllWindows()
+
+# Create a new, resized image and save
+resized_image = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
+cv2.imshow('Hexacopter', resized_image)
+cv2.imwrite('resource_files/Tarot680_Resized.jpg', resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
