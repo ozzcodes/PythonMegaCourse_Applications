76afe3095d431ee9d845e93abafc43e017db9269
OzzyCodes <ajwaldro@gmail.com>
2019-10-21 13:03:54 -0400

    video capturing using opcv

--- PythonBasics/Image_Video_Processing/video_capturing.py
+++ PythonBasics/Image_Video_Processing/video_capturing.py
@@ -5,8 +5,20 @@ import time
 # First camera, specify index if multiple camera's
 my_video = cv2.VideoCapture(0)
 
+# Print a frame, after checking it's active
+check, frame = my_video.read()
+print(check)
+print(frame)
+
 # Allow the camera to be used for specified time
+# Also create a gray image just for testing
+gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 time.sleep(3)
 
+# Show the image on a frame
+cv2.imshow('Video Capture', gray_scale)
+
 # Allow computer to release imaging from camera
+cv2.waitKey(0)
 my_video.release()
+cv2.destroyAllWindows()
